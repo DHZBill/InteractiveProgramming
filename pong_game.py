96c35a37e46513b32603c3973cf305d915511edb
Bill <Bill>
2016-04-18 21:05:47 -0400

    Modified

--- pong_game.py
+++ pong_game.py
@@ -6,12 +6,7 @@ import math
 from multiprocessing import Process, Pipe
 import track
 
-""" Questions/Commentary Section """
-# weird bug in scoring system that makes the first bounce count for 2 pts
-# haven't looked into it yet ...
-
 size = (1080, 720)
-
 
 class PyGameBrickBreakerView(object):
     """ Provides a view of the brick breaker model in a pygame
@@ -28,12 +23,6 @@ class PyGameBrickBreakerView(object):
         self.screen.fill(pygame.Color('black'))
 
         text_init = pygame.font.SysFont("UbuntuBold", 100)
-
-        # left_score = text_init.render(str(self.model.score.left), 1, (255,255,255))
-        # self.screen.blit(left_score, (size[0]/2 + 100, 15))
-
-        # right_score = text_init.render(str(self.model.score.right), 1, (255,255,255))
-        # self.screen.blit(right_score, (size[0]/2 - 150, 15))
 
         for wall in self.model.walls:
             r = pygame.Rect(wall.left,
@@ -54,12 +43,17 @@ class PyGameBrickBreakerView(object):
                            (int(self.model.ball.center_x + 0.5), int(self.model.ball.center_y + 0.5)),
                            self.model.ball.radius)
 
-        if center_parent.recv() != None:
+        if center_parent.recv():
 
-            self.model.paddle1.top = (center_parent.recv()[1]-240) * 2 +240 +100
-            self.model.paddle2.top = (center_parent.recv()[3]-240) * 2 +240 +100
+            # get the vertical positions of paddles
+            position_1 = center_parent.recv()[1]
+            position_2 = center_parent.recv()[3]
 
+            # adjust the position of paddles on pygame window
+            self.model.paddle1.top = (position_1-240) * 2 + 350
+            self.model.paddle2.top = (position_2-240) * 2 + 350
 
+        # draw paddles at the right positions
         r1 = pygame.Rect(self.model.paddle1.left,
                         self.model.paddle1.top - self.model.paddle1.height/2,
                         self.model.paddle1.width,
@@ -71,18 +65,12 @@ class PyGameBrickBreakerView(object):
         pygame.draw.rect(self.screen, pygame.Color('orange'), r1)
         pygame.draw.rect(self.screen, pygame.Color('blue'), r2)
 
-
+        # add scores
         left_score = text_init.render(str(self.model.score.left), 1, (255,255,255))
         self.screen.blit(left_score, (size[0]/2 + 100, 15))
 
         right_score = text_init.render(str(self.model.score.right), 1, (255,255,255))
         self.screen.blit(right_score, (size[0]/2 - 150, 15))
-
-        # if self.ball.center_x <=1 or self.ball.center_x >= self.paddle2.left + 10:
-        #     self.score.right += 1 
-
-
-
         
         pygame.display.update()
 
@@ -115,44 +103,60 @@ class BrickBreakerModel(object):
         self.score = Score(0, 0)
 
         self.ball = Ball(width/2, (height/2 + self.WALL1_TOP/2), self.BALL_RADIUS)
-        self.paddle1 = Paddle(5, (height/2 + self.WALL1_TOP/2))
-        self.paddle2 = Paddle(width - 15, (height/2 + self.WALL1_TOP/2))
+        self.paddle1 = Paddle(0)
+        self.paddle2 = Paddle(width - 15)
+
+    def bounce(self, paddle):
+        """ Determine the velocity of ball after collision happens """
+
+        angle = 180 - (self.ball.center_y - paddle.top)/self.paddle1.height*180
+        self.ball.velocity_x = abs(math.cos(math.radians(angle)) * self.ball.speed)
+        self.ball.velocity_y = abs(math.sin(math.radians(angle)) * self.ball.speed)
+        
+        # set the minimum speed on x direction
+        speed_limit_x = 5
+        if self.ball.velocity_x <= speed_limit_x:
+            self.ball.velocity_x = speed_limit_x
+
+        if paddle == self.paddle1:
+            self.ball.center_x = self.paddle1.left + self.paddle1.width +1
+            self.score.right += 1
+            print 'paddle1 hit'
+
+        else:
+            self.ball.velocity_x = -self.ball.velocity_x
+            self.ball.center_x = self.paddle2.left-1
+            self.score.left += 1
+            print 'paddle2 hit'
+
+
 
     def collision(self):
+        """ Detect collisions """
+        # set the boundaries in which the ball should bounce back
+        boundaryX_1 = (self.paddle1.left + self.paddle1.width/2 - 10, 
+                      self.paddle1.left + self.paddle1.width)        
+        boundaryX_2 = (self.paddle2.left, 
+                      self.paddle2.left + self.paddle2.width/2 + 10)        
+        boundaryY_1 = (self.paddle1.top - self.paddle1.height/2, 
+                      self.paddle1.top + self.paddle1.height/2)        
+        boundaryY_2 = (self.paddle2.top - self.paddle2.height/2, 
+                      self.paddle2.top + self.paddle2.height/2)        
+        boundary_Wall = (self.WALL1_TOP + self.WALL_HEIGHT, 
+                        self.WALL2_TOP - self.WALL_HEIGHT)
 
-        if self.ball.center_x <= self.paddle1.left + self.paddle1.width and self.ball.center_x >= self.paddle1.left + self.paddle1.width/2 - 10:
-            if self.ball.center_y >= self.paddle1.top - self.paddle1.height/2 and self.ball.center_y <= self.paddle1.top + self.paddle1.height/2:
-                n = 180 - (self.ball.center_y - (self.paddle1.top))/self.paddle1.height*180
-                self.bounce(n)
-                if self.ball.velocity_x <= 5:
-                    self.ball.velocity_x = 5
-                print 'panel1 hit'
-                self.ball.center_x = self.paddle1.left + self.paddle1.width +1
+        # detect collision between ball and paddles
+        if self.ball.center_x >= boundaryX_1[0] and self.ball.center_x <= boundaryX_1[1]:
+            if self.ball.center_y >= boundaryY_1[0] and self.ball.center_y <= boundaryY_1[1]:
+                self.bounce(self.paddle1)
 
-                self.score.right += 1
-
-        elif self.ball.center_x >= self.paddle2.left and self.ball.center_x <= self.paddle2.left + self.paddle2.width/2 + 10:
-            if self.ball.center_y >= self.paddle2.top - self.paddle2.height/2  and self.ball.center_y <= self.paddle2.top + self.paddle2.height/2:
-                n = 180 - (self.ball.center_y - (self.paddle2.top))/self.paddle2.height*180
-                self.bounce(n)
-                if self.ball.velocity_x <= 5:
-                    self.ball.velocity_x = 5
-                self.ball.velocity_x = -self.ball.velocity_x
-                print self.score.left
-                print 'panel2 hit', self.ball.center_y, (self.paddle2.top)
-                self.ball.center_x = self.paddle2.left-1
-                self.score.left += 1
-
-
-
-        if self.ball.center_y <= (self.WALL1_TOP + self.WALL_HEIGHT) or self.ball.center_y >= self.WALL2_TOP:
-            self.ball.velocity_y = -self.ball.velocity_y
-
-
-    def bounce(self, n):
+        elif self.ball.center_x >= boundaryX_2[0] and self.ball.center_x <= boundaryX_2[1]:
+            if self.ball.center_y >= boundaryY_2[0]  and self.ball.center_y <= boundaryY_2[1]:
+                self.bounce(self.paddle2)
         
-        self.ball.velocity_x = abs(math.cos(math.radians(n)) * self.ball.speed)
-        self.ball.velocity_y = abs(math.sin(math.radians(n)) * self.ball.speed)
+        # detect collision between ball and wall
+        if self.ball.center_y <= boundary_Wall[0] or self.ball.center_y >= boundary_Wall[1]:
+            self.ball.velocity_y = -self.ball.velocity_y
 
     def update(self):
         """ Update the model state """
@@ -162,12 +166,11 @@ class BrickBreakerModel(object):
 
 class Paddle(object):
     """ Represents the paddle in the game """
-    def __init__(self, left, top):
+    def __init__(self,left):
         self.left = left
-        self.top = top
+        self.top = 0
         self.width = 10
         self.height = 100
-
 
 
 class Ball(object):
@@ -178,7 +181,7 @@ class Ball(object):
         self.center_y = center_y
 
         self.radius = radius
-        self.velocity_x = random.choice([10,-10])         # pixels / frame
+        self.velocity_x = random.choice([20,-20])         # pixels / frame
         self.velocity_y = 0         # pixels / frame
         self.speed = math.sqrt(self.velocity_x **2 + self.velocity_y **2)
 
@@ -186,7 +189,6 @@ class Ball(object):
         """ Update the position of the ball due to time passing """ 
         self.center_x += self.velocity_x
         self.center_y += self.velocity_y
-
 
 
 class Wall(object):
@@ -198,9 +200,10 @@ class Wall(object):
         self.top = top
         self.width = width
         self.height = height
-        #self.color = color
 
-class Dash(object):
+
+class Dash(object):            # adjust the position of paddles on pygame window
+
     """ Represents a dash in my pong game """
     def __init__(self, left, top, width, height):
         """ Initializes a Dash object with the specified
@@ -210,84 +213,29 @@ class Dash(object):
         self.width = width
         self.height = height
 
+
 class Score(object):
     """ Represents the score in my pong game """
     def __init__(self, left, right):
         """ Initializes a Score object with the specified
             left and right values """
         self.left = left
-        self.right = right
-
-class PyGameKeyboardController(object):
-    def __init__(self, model, size):
-        self.model = model
+        self.right = right            # adjust the position of paddles on pygame window
 
 
 
-    
-    def handle_event(self,event):
-        keystate = pygame.key.get_pressed()
-        jump = 20
-        margin = 50
-        upper_wall = 110
-
-        """ Looks for w & s keypresses to move the y position of Paddle 1
-        if Paddle is in range of board""" 
-        if event.type != KEYDOWN:
-            return
-        if event.key == pygame.K_w:
-            if jump+margin + upper_wall < self.model.paddle1.top:
-                self.model.paddle1.top -= jump
-            else:
-                return
-        if event.key == pygame.K_s:
-            if self.model.paddle1.top < size[1] - (jump + margin + 10):
-                self.model.paddle1.top -= -jump
-            else:
-                return
-
-
-        """ Looks for UP & DOWN keypresses to move the y position of Paddle 2
-        if Paddle is in range of board"""
-        if event.type != KEYDOWN:
-            return
-        if event.key == pygame.K_UP:
-            if jump + margin + upper_wall < self.model.paddle2.top:
-                self.model.paddle2.top -= jump
-            else:
-                return
-        if event.key == pygame.K_DOWN:
-            if self.model.paddle2.top < size[1] - (jump + margin + 10):
-                self.model.paddle2.top -= -jump
-            else:
-                return
 def run(center_parent):
     pygame.init()
     model = BrickBreakerModel(size[0], size[1])
     view = PyGameBrickBreakerView(model, size)
-    controller = PyGameKeyboardController(model, size)
     running = True
     while running:
-        
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-            else:
-                controller.handle_event(event)
         model.update()
         view.draw()
-        # print center_parent.recv()
-        # print model.paddle2.top
-        # print model.ball.center_y
-        # print model.ball.velocity_x
-        # time.sleep(0.001)
-if __name__ == '__main__':
-    # pygame.init()
-    # size = (1080, 720)
 
-    # model = BrickBreakerModel(size[0], size[1])
-    # view = PyGameBrickBreakerView(model, size)
-    # controller = PyGameKeyboardController(model, size)
+if __name__ == '__main__':
+    
+    size = (1080, 720)
     center_parent, center_child = Pipe()
     q = Process(target=track.Tracking, args=(center_child,))
     p = Process(target=run, args=(center_parent,))
